3751f78c4a7a5ef56abcfe326aa81604927affdf
KoSi <otpog23@gmail.com>
2025-09-12 13:23:38 +0000

    Added(View Product page): [Product info section]

--- src/pages/ViewProduct.tsx
+++ src/pages/ViewProduct.tsx
@@ -38,8 +38,90 @@ export function ViewProduct() {
                         </div>
                     </div>
 
-                    <div>
+                    <div className='info'>
+                        <div className="type">
+                            MAKEUP
+                        </div>
 
+                        <div style={{fontFamily: 'Caveat'}} className="name">
+                            Luxury Foundation - Medium Coverage
+                        </div>
+
+                        <div className="by">
+                            by <span>GlowGoals</span>
+                        </div>
+
+                        <div className="ratings-reviews">
+                            <div className="ratings">
+                                <svg fill="currentColor" width="20" height="20" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+
+                                <svg fill="currentColor" width="20" height="20" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+
+                                <svg fill="currentColor" width="20" height="20" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+
+                                <svg fill="currentColor" width="20" height="20" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+
+                                <svg fill="currentColor" width="20" height="20" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            </div>
+
+                            <div className="reviews">
+                                4.6 (89 reviews)
+                            </div>
+                        </div>
+
+                        <div className="price">
+                            GHC 550
+                        </div>
+
+                        <div className="description">
+                            Long-lasting foundation with SPF 30 for flawless, natural-looking coverage
+                        </div>
+
+                        <div className="details">
+                            <div style={{fontFamily: 'Caveat'}} className="head">
+                                Product Details
+                            </div>
+
+                            <div className="sub">
+                                <div className="brand">
+                                    Brand: <span>GlowGoals</span>
+                                </div>
+
+                                <div>
+                                    Category: <span>Makeup</span>
+                                </div>
+                            </div>
+
+                            <div className="sub">
+                                <div className="brand">
+                                    Skin Type: <span>All Type</span>
+                                </div>
+
+                                <div>
+                                    Stock: <span>18 units</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="add-btn">
+                            <button>
+                                Add to Cart
+                            </button>
+                        </div>
+
+                        <div className="btns">
+                            <button id='wish'>
+                                <svg fill="none" stroke="currentColor" width="16" height="16" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+
+                                Add to Wishlist
+                            </button>
+
+                            <button id='cart'>
+                                <svg fill="none" stroke="currentColor" width="16" height="16" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
+
+                                Buy Now
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
