5c9d2bcb432cb5d534f91bb057a2eecc2ffe5bf3
KoSi <otpog23@gmail.com>
2025-09-13 22:26:12 +0000

    Added(View Product page): [Toggle send review section]

--- src/pages/ViewProduct.tsx
+++ src/pages/ViewProduct.tsx
@@ -4,6 +4,7 @@ import '../styles/ViewProduct.css'
 export function ViewProduct() {
     const [rating, setRating] = useState<number>(0);
     const [review, setReview] = useState<string>('')
+    const [sendReview, setSendReview] = useState<boolean>(false);
 
     const stars = [1, 2, 3, 4, 5];
 
@@ -238,7 +239,7 @@ export function ViewProduct() {
                             </div>
                         </div>
 
-                        <button>
+                        <button onClick={() => setSendReview((s) => !s)}>
                             <svg width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
 
                             Write a Review
@@ -246,7 +247,7 @@ export function ViewProduct() {
                     </div>
                 </div>
 
-                <div className="send-review">
+                {sendReview && <div className="send-review">
                     <div style={{fontFamily: 'Caveat'}} id="head">
                         <svg width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
 
@@ -320,11 +321,11 @@ export function ViewProduct() {
                             Submit Review
                         </button>
 
-                        <button>
+                        <button onClick={() => setSendReview((s) => !s)}>
                             Cancel
                         </button>
                     </div>
-                </div>
+                </div>}
 
                 <div style={{fontFamily: 'Caveat'}} id="may-like">
                     You may also like
